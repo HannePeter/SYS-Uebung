9783385de4ec7a2035fc45f81a66b07afb694c10
HannePeter <hannepeter@posteo.de>
2018-05-11 18:08:18 +0200

    sysinfo

--- sysinfo.sh
+++ sysinfo.sh
@@ -6,3 +6,18 @@
 # - Größe und verfügbarer Platz der „/home “-Partition (falls vorhanden)
 # - Die Anzahl laufender Prozesse
 # - Name des laufenden Betriebssystems inklusive Kernelversion
+
+#!/bin/bash
+
+echo "CPU:           $(lscpu | grep "Model name" | cut -c 22-)"
+
+echo "RAM:           total: $(free -m | grep Mem | cut -c 13-43) MeBi"
+echo "               free:  $(free -m | grep Mem | cut -c 13-43)"
+
+echo "ROOT:          $(df -h | grep "/dev/sda4")"
+
+echo "HOME:          $(df -h | grep "/home")"
+
+echo "Anz. Prozesse: $(ps -e | wc -l)"
+
+echo "OS/Kernel:     $(uname -or)"
