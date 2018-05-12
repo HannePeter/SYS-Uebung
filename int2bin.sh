6fd21e817c48a731fa537e12689a9829908cba6b
HannePeter <hannepeter@posteo.de>
2018-05-12 16:30:28 +0200

    int2bin

--- int2bin.sh
+++ int2bin.sh
@@ -1,2 +1,14 @@
 # Schreiben Sie ein Bash-Skript int2bin.sh welches eine übergeben Ganzzahl als
 # Bitstring ausgibt.
+
+
+#!/bin/bash
+
+number=$1
+
+while [ $number -gt 0 ]; do
+    bitstring="$((number % 2))$bitstring"
+    number=$((number / 2))
+done
+
+echo "Bitstring: $bitstring"
