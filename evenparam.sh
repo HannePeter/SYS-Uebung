9bbde3f93d81fe636ad087159d13862ec166169e
HannePeter <hannepeter@posteo.de>
2018-05-12 20:24:15 +0200

    evenparam

--- evenparam.sh
+++ evenparam.sh
@@ -8,3 +8,25 @@
 # Geben Sie eine Usage-Meldung auf stderr aus und beenden Sie das Skript
 # mit dem Rückgabewert 1, falls das Programm das Programm nicht sachgemäß
 # aufgerufen wurde.
+
+
+#!/bin/bash
+
+isEven=$(($# % 2))
+
+if [ $isEven -eq 0 ]; then
+    parameter=$@
+    loadFirst=0
+    for element in $parameter; do
+        if [ $loadFirst -eq 1 ]; then
+            echo "$first = $element"
+            loadFirst=0
+        else
+            first=$element
+            loadFirst=1
+        fi
+    done
+else
+    echo "Es wird eine gerade Anzahl an Parametern benötigt" >&2 
+    exit 1
+fi
