29cf42103547567ddd5373ecd39ded24bcd9557b
HannePeter <hannepeter@posteo.de>
2018-05-13 13:27:33 +0200

    last2

--- last2.sh
+++ last2.sh
@@ -1,2 +1,15 @@
 # Schreiben Sie ein Shell-Skript last2.sh, das nur die letzten beiden Komman-
 # dozeilenparameter ausgibt.
+
+
+#!/bin/bash
+
+parameter=$@
+
+for element in $parameter; do
+    beforeLast=$last
+    last=$element
+done
+
+echo "Letzter Parameter:    $last"
+echo "Vorletzter Parameter: $beforeLast"
